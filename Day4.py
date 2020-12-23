e20c91f9d8e4e1d77fc28d71e4dca1786a0794a9
uhalisdemir <ufuk.halisdemir@altran.com>
2020-12-23 09:25:32 +0100

    Day4
    Signed-off-by: uhalisdemir <ufuk.halisdemir@altran.com>

--- Day4.py
+++ Day4.py
@@ -5,11 +5,20 @@ Created on Fri Dec 18 15:31:58 2020
 @author: uhalisdemir
 """
 
-with open("Data/inputd4.txt", "r") as r:
-    with open("Data/inputd4new.txt", "w") as w:
-        w.write(r.read().replace('\n\n', ','))
-
 import pandas as pd
 
-# df = pd.read_csv('Data/inputd4.txt', header=None, sep=" ", names=["input"])
-# dfn = pd.read_csv('Data/inputd4new.txt', header=None, sep=" ", names=["input"])
+# Passport on single line
+# with open("Data/a.txt", "r") as r:
+#     with open("Data/inputd4new.txt", "w") as w:
+#         w.write(r.read().replace('\n\n', '\nFakeline\n').replace('\n', ' ').replace(' Fakeline ', '\n'))
+
+# create regex pattern to look for
+keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+pattern = ""
+for el in keys:
+    pattern += "(?=.*{})".format(el)
+
+
+# Load cleaned file
+df = pd.read_csv('Data/inputd4new.txt', header=None,names=['PassID'])
+print(df['PassID'].str.contains(pattern).sum())
